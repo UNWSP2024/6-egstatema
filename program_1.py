5d05549a995bda7848c842e456003a2369a88a65
egstatema <egstatema@students.unwsp.edu>
2025-02-27 16:56:27 -0600

    Add files via upload

--- program_1.py
+++ program_1.py
@@ -1,15 +1,17 @@
+# Eliya Statema
+# 2/27/25
+# Random Dice
 
-# Program #1: Random Dice
-# Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
-# The dice sum will be the output of this function.
+import random
 
-def randDice():
-    # Write your logic to generate 2 numbers between 1 and 6 here
+def ranDice():
+    die_1 = random.randint(1, 6)
+    die_2 = random.randint(1, 6)
+    return die_1 + die_2
 
-    # Sum 2 numbers
-
-    # return sum to calling function
-
-#########
-# Then write a mainline that calls the "randDice" function 100 times in a for loop.  
-# The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+rolls_sum = 0
+for loop in range(100):
+    rolls_sum += ranDice()
+    
+average = rolls_sum / 100
+print(f"The average of two dice rolled 100 times is {average:.2f}.")
---
+++ pseudocode_2.py
@@ -0,0 +1,12 @@
+# Eliya Statema
+# 2/27/25
+# Monthly Tax Rate Pseudocode
+
+# PRINT instructions
+# -------FUNCTION-------
+# Assign tax rates to variables
+# INPUT total sales for the month
+# CALCULATE county sales tax, state sales tax, and total sales tax
+# RETURN taxes
+# ----------------------
+# PRINT result
